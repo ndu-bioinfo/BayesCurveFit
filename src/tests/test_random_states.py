36e0e1ae215b0e6d5e58b3fe492c760ffc384f0e
niudu85 <jiyin.dna@gmail.com>
2025-02-09 17:57:25 -0800

    test rand

--- src/tests/test_random_states.py
+++ src/tests/test_random_states.py
@@ -3,16 +3,15 @@ import numpy as np
 
 class TestRandomState(unittest.TestCase):
     def test_random_state(self):
-        expected_array = np.array([
-            42, 3107752595, 1895908407, 3900362577, 3030691166,
-            4081230161, 2732361568, 1361238961, 3961642104, 867618704
+        expected_array = np.array([2147483648
         ], dtype=np.uint32)
         
         # Create the RandomState object and get the state
-        state = np.random.RandomState(seed=42).get_state()[1][:10]
+        state = np.random.RandomState().get_state()[1][:1]
         
         # Assert that the state matches the expected values
         np.testing.assert_array_equal(state, expected_array)
+
 
 if __name__ == '__main__':
     unittest.main()
